b32ebdaa12c41147eb057d70f884c3a3f6822bac
Dani <prihoczki.daniel@gmail.com>
2023-07-31 18:25:38 +0200

    now we can see and actual working clock

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -8,6 +8,7 @@ public class MyFrame extends JFrame {
     Calendar calendar;
     SimpleDateFormat timeFormat;
     JLabel timeLabel;
+    String time;
 
     public MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,8 +17,28 @@ public class MyFrame extends JFrame {
         this.setSize(350,200);
         this.setResizable(true);
 
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
+        timeLabel = new JLabel();
 
 
+
+        this.add(timeLabel);
         this.setVisible(true);
+
+        setTime();
+
+
+    }
+    public void setTime(){
+        while(true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
