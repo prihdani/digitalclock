dcdd5a48055f3ca3ff4943782b2e7029f19cb2ee
Dani <prihoczki.daniel@gmail.com>
2023-07-31 18:15:25 +0200

    Created basic screen that displays nothing yet

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+
+    public MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Digital Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350,200);
+        this.setResizable(true);
+
+
+
+        this.setVisible(true);
+    }
 }
