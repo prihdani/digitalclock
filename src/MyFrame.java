41ecc9ce6b7bfd6e556b4f93445be2af804ecdfe
Dani <prihoczki.daniel@gmail.com>
2023-07-31 18:37:33 +0200

    finished

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -7,23 +7,44 @@ public class MyFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
     String time;
+    String day;
+    String date;
 
     public MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Digital Clock");
         this.setLayout(new FlowLayout());
         this.setSize(350,200);
-        this.setResizable(true);
+        this.setResizable(false);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
 
         timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
+        timeLabel.setForeground(Color.pink);
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+
+        dateLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdana",Font.PLAIN,25));
+
 
 
 
         this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
         this.setVisible(true);
 
         setTime();
@@ -34,6 +55,13 @@ public class MyFrame extends JFrame {
         while(true) {
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
